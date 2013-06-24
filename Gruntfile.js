5e47aab5d06a11b4a5c24dedc244253bb6591e5a
Justin <yuting1987@gmail.com>
2013-06-24 11:56:00 +0800

    Revert "Update"
    This reverts commit 3f60710aa37b2211f7eedef11ecd3cee000a8e01.

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,37 +38,25 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         options:{
-          beautify: false,
-          report: false,
-          mangle: {
-            except: ['$scope','$cookieStore']
-          }
+          beautify: true
         },
         files: {
-          './script/page.js': ['./dev/script/helper*.js','./dev/script/page.js'],
-        }
-      }
-    },
-    cssmin: {
-      combine: {
-        files: {
-          './style/page.css': ['./dev/style/reset.css','./dev/style/bootstrap.css','./dev/style/page.css']
+          './script/page.js': ['./dev/script/helper*.js','./dev/script/page.js']
         }
       }
     },
     watch: {
       files: ['**/*.jade','**/*.coffee','**/*.sass'],
-      tasks: ['jade','coffee','uglify','compass','cssmin']
+      tasks: ['jade','coffee','uglify','compass']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jade','coffee','uglify','compass','cssmin','watch']);
+  grunt.registerTask('default', ['jade','coffee','uglify','compass','watch']);
 
 };
